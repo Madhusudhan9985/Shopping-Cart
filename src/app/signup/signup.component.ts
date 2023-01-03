8f22236fef56b1129d69ed34617ec12af88ddd6a
Kalle Madhusudhan <Kalle.Madhusudhan@marlabs.com>
2023-01-03 13:04:38 +0530

    Initial commit

---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent {
+    title = 'angularpopup';
+    showModal: boolean;
+    registerForm: FormGroup;
+    submitted = false;
+    constructor(private formBuilder: FormBuilder,
+      ) { this.registerForm}
+    show()
+    {
+      this.showModal = true; // Show-Hide Modal Check
+      
+    }
+    //Bootstrap Modal Close event
+    hide()
+    {
+      this.showModal = false;
+    }
+  
+    ngOnInit() {
+      this.registerForm = this.formBuilder.group({
+          email: ['', [Validators.required, Validators.email]],
+          password: ['', [Validators.required, Validators.minLength(6)]],
+          firstname: ['', [Validators.required, Validators.minLength(6)]],
+          mobile: ['', [Validators.required, Validators.pattern("[0-9]*"), Validators.minLength(10)]]
+      });
+  }
+
+  
+
+  // convenience getter for easy access to form fields
+  get f() { return this.registerForm.controls; }
+  onSubmit() {
+      this.submitted = true;
+      // stop here if form is invalid
+      if (this.registerForm.invalid) {
+          return;
+      }
+      if(this.submitted)
+      {
+        this.showModal = false;
+      }
+     
+  }
+    
+  }
